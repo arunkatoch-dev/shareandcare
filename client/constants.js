9aa478355845edf0f7aba105384eac27a4607511
Arun Katoch <arunkatoch.dev@gmail.com>
2024-10-12 15:48:39 +0530

    edit URls

--- client/constants.js
+++ client/constants.js
@@ -1,4 +1,4 @@
-export const backendURL = "https://shareandcare.onrender.com/"
-export const frontendURL = "https://shareandcare.vercel.app/"
+export const backendURL = "https://shareandcare.onrender.com"
+export const frontendURL = "https://shareandcare.vercel.app"
 // "http://localhost:5173"
 // http://localhost:5000";
